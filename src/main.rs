2fe7b0ccab6fadad8f236b38cc40a8f6d2dd6cbf
Simon THOBY <git@nightmared.fr>
2021-02-20 23:26:48 +0100

    EDIT: the previous issue was probably due to a skylake optimization that distinguishes between 0 and non-zero values (see Travis articles). Should work much better now

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ const MULTIPLE_OFFSET: usize = 4096;
 const NB_TRIES: usize = 17;
 
 static TO_LEAK: usize = 0x0123456789abcdef;
-static mut TEST_ARR: [u8; 256 * MULTIPLE_OFFSET + 64] = [0; 256 * MULTIPLE_OFFSET + 64];
+static mut TEST_ARR: [u8; 256 * MULTIPLE_OFFSET + 64] = [127; 256 * MULTIPLE_OFFSET + 64];
 static mut HISTOGRAM: [[u64; NB_TRIES]; 256] = [[0; NB_TRIES]; 256];
 
 unsafe fn measure_addr(target_adress: *const u8) -> u8 {
@@ -28,23 +28,21 @@ unsafe fn measure_addr(target_adress: *const u8) -> u8 {
         }
 
         // perform the access
-        asm!("movzx rax, byte ptr [{0}]", "imul rax, {2}", "add {1}, rax", "mov qword ptr [{1}], $1", in(reg) target_adress, inout(reg) base_addr => _, in(reg) MULTIPLE_OFFSET, out("rax") _);
-        for i in 0..((256 * MULTIPLE_OFFSET) / 64) {
-            core::arch::x86_64::_mm_clflush((base_addr as usize + i * 64) as *const u8 as *mut u8);
-        }
+        asm!("movzx rax, byte ptr [{0}]", "imul rax, {2}", "add {1}, rax", "movzx rax, byte ptr [{1}]", in(reg) target_adress, inout(reg) base_addr => _, in(reg) MULTIPLE_OFFSET, out("rax") _);
+
         for i in 0..256 {
-            let count_addr = base_addr as usize + v[i] * MULTIPLE_OFFSET;
+            let count_addr = base_addr as usize + i * MULTIPLE_OFFSET;
             let delta_tsc;
             // measure the time
             let mut res: usize;
             asm!("mfence", "lfence", "rdtsc", "lfence", "shl rdx, $32", "or rax, rdx", "mov rcx, rax",
                  "movzx r9, byte ptr [{0}]",
-                 "mfence", "lfence", "rdtsc", "lfence", "shl rdx, $32", "or rax, rdx", "sub rax, rcx",
+                 "mfence", "lfence", "rdtsc", "shl rdx, $32", "or rax, rdx", "sub rax, rcx",
                  in(reg) count_addr,
                  out("rdx") _, out("rcx") _, out("rax") delta_tsc, out("r9") res);
             println!("{}", std::ptr::read_volatile(count_addr as *const u8));
             println!("{}", res);
-            HISTOGRAM[v[i]][j] = delta_tsc;
+            HISTOGRAM[i][j] = delta_tsc;
         }
     }
 
